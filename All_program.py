63290ac363ce730a1850111e1b56f57bfb725177
miller <miller_tamil@live.com>
2015-11-09 15:45:26 +0530

    Update All_program.py

--- All_program.py
+++ All_program.py
@@ -2,3 +2,12 @@
 #Empty line remover
 sed -e '/^[[:blank:]]*$/d' source_file > newfile
 
+#Frequency word count.
+files = list_of_files
+fd = nltk.FreqDist()
+for file in files:
+    with open(file) as f:
+        for sent in nltk.sent_tokenize(f.lower()):
+            for word in nltk.word_tokenize(sent):
+                fd.inc(word)
+
